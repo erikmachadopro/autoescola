7c17696da474d8d5ce71ace26477becb13a6b164
erikmachadopro <erikmachadopro@gmail.com>
2022-10-19 20:24:31 -0300

    Controllers, Models. Redirecionando para paineis

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -2,9 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\usuario;
 use Illuminate\Http\Request;
 
 class UsuarioController extends Controller
 {
-    //
+    public function login(Request $request){
+        
+        $usuario = $request->usuario;
+        $senha = $request->senha;
+
+        $usuarios = usuario::where('usuario', '=', $usuario)->where('senha', '=', $senha)->first();
+        
+        if(@$usuarios->id != null){
+            @session_start();
+            $_SESSION['id_usuario'] = $usuarios->id;
+            $_SESSION['nome_usuario'] = $usuarios->nome;
+            $_SESSION['nivel_usuario'] = $usuarios->nivel;
+            $_SESSION['cpf_usuario'] = $usuarios->cpf;
+            
+            if($_SESSION['nivel_usuario'] == 'admin'){
+                return view('painel-admin.index');
+            }
+
+            if($_SESSION['nivel_usuario'] == 'instrutor'){
+                return view('painel-instrutor.index');
+            }
+
+            if($_SESSION['nivel_usuario'] == 'recepcionista'){
+                return view('painel-recepcao.index');
+            }
+
+        }else{
+            echo "<script language='javascript'> window.alert('Dados Incorretos!') </script>";
+            return view('home');
+        }  
+    }
+    public function logout(){
+       @session_start();
+       @session_destroy();
+       return view('home');
+    }
 }
